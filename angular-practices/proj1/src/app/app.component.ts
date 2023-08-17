54b27a6a27786983a0d4d9777b0431bdab7f6df5
İlker YILMAZ <53461108+ilker-yilmaz@users.noreply.github.com>
2023-08-17 15:32:12 +0300

    .

--- angular-practices/proj1/src/app/app.component.ts
+++ angular-practices/proj1/src/app/app.component.ts
@@ -4,11 +4,20 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   //templateUrl: './app.component.html',
   template: `<h1>merhaba</h1>
-   <app-home></app-home>
-    <app-com1></app-com1>`,
+   <app-home [pageName]="pageName"></app-home>
+    <app-com1></app-com1>
+    <p>{{name}}</p>
+    <img [src]="src" [width]="width">
+    <br>
+    <input type="text" [value]="title">
+    `,
   //styleUrls: ['./app.component.scss']
   styles: ['h1{background-color:red}']
 })
 export class AppComponent {
+  src:string="https://i.pinimg.com/564x/cd/b4/cf/cdb4cf750833121feabcd18cab45fb1a.jpg"
+  width:number = 500
+  name: string = "ilker"
+  pageName: string = 'Home Page';
   title = 'proj1';
 }
--- angular-practices/proj1/src/app/home/home.component.ts
+++ angular-practices/proj1/src/app/home/home.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
+  // templateUrl: './home.component.html',
+  template: `
+  <h1>home component</h1>
+  <h1>{{pageName}}</h1>
+  `,
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+  @Input() pageName: string;
 }
