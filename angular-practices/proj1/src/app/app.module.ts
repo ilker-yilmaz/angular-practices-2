14c5b89703452c0c8498af8f12075557bed732b0
İlker YILMAZ <53461108+ilker-yilmaz@users.noreply.github.com>
2023-08-18 18:08:28 +0300

    login component added

--- angular-practices/proj1/src/app/app.module.ts
+++ angular-practices/proj1/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FormsModule } from '@angular/forms';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
