f741b536c3535d487385e4840d40975e645f1845
İlker YILMAZ <53461108+ilker-yilmaz@users.noreply.github.com>
2023-08-18 18:07:45 +0300

    com1 component deleted

--- angular-practices/proj1/src/app/app.module.ts
+++ angular-practices/proj1/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { Com1Component } from './com1/com1.component';
 import { FormsModule } from '@angular/forms';
 import { NavbarComponent } from './navbar/navbar.component';
 
@@ -12,7 +11,6 @@ import { NavbarComponent } from './navbar/navbar.component';
   declarations: [
     AppComponent,
     HomeComponent,
-    Com1Component,
     NavbarComponent
   ],
   imports: [
