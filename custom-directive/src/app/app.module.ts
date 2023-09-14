95972f8f1c90e47f7e1874786128116259662c16
122E337 <53461108+ilker-yilmaz@users.noreply.github.com>
2023-09-14 19:55:19 +0300

    Custom Directive

--- custom-directive/src/app/app.module.ts
+++ custom-directive/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExampleDirective } from './directives/example.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExampleDirective
   ],
   imports: [
     BrowserModule,
