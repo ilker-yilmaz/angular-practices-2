33f69a0f95a29a2384d018389a3f074e3aafd0ed
122E337 <137714673+123R008project@users.noreply.github.com>
2023-09-18 18:57:22 +0300

    structural directives

--- structural-directive/src/app/app.component.ts
+++ structural-directive/src/app/app.component.ts
@@ -2,9 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <!-- <div *appCustomif="true">
+    merhaba
+  </div> -->
+  <!-- <ul>
+    <li *appCustomfor="5; let i = index">ilker{{i}}</li>
+  </ul> -->
+  <ul>
+    <li *appCustomfor="names; let name; let index = index">{{name}}-{{index}}</li>
+  </ul>
+  `,
 })
 export class AppComponent {
   title = 'structural-directive';
+  names: string[] = ['ilker', 'hasan', 'ahmet', 'fırat', 'mert', 'resul', 'furkan'];
 }
